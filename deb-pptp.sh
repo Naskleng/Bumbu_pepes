ecdec8634ed39b4307101a0d7cd3b3f0cd784037
NetzoneSSH <meinaki@hackermail.com>
2014-06-25 14:07:03 +0700

    Update deb-pptp.sh

--- deb-pptp.sh
+++ deb-pptp.sh
@@ -1,58 +1,138 @@
-#!/bin/bash -x
+#!/bin/bash
+# Interactive PoPToP install script for an OpenVZ VPS
+# Tested on Debian 5, 6, and Ubuntu 11.04
+# April 2, 2013 v1.11
+# http://www.putdispenserhere.com/pptp-debian-ubuntu-openvz-setup-script/
 
-#
-# drewsymo/VPN
-#
-# Installs a PPTP VPN-only system for CentOS
-#
-# @package VPN 2.0
-# @since VPN 1.0
-# @author Drew Morris
-#
+echo "######################################################"
+echo "Interactive PoPToP Install Script for an OpenVZ VPS"
+echo
+echo "Make sure to contact your provider and have them enable"
+echo "IPtables and ppp modules prior to setting up PoPToP."
+echo "PPP can also be enabled from SolusVM."
+echo
+echo "You need to set up the server before creating more users."
+echo "A separate user is required per connection or machine."
+echo "######################################################"
+echo
+echo
+echo "============================================"
+echo "Netzone PPTP Script Installer By Danyjrx"
+echo "Pilh Salah Satu:"
+echo "1) Install PoPToP server dan buat satu user"
+echo "2) Membuat User"
+echo "============================================"
+read x
+if test $x -eq 1; then
+	echo "Enter username that you want to create (eg. client1 or john):"
+	read u
+	echo "Specify password that you want the server to use:"
+	read p
 
-(
+# get the VPS IP
+ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
 
-VPN_IP=`curl ipv4.icanhazip.com>/dev/null 2>&1`
+echo
+echo "===================================="
+echo "Lagi download sama install PoPToP"
+echo "===================================="
+apt-get update
+apt-get -y install pptpd
 
-VPN_USER="myuser"
-VPN_PASS="mypass"
+echo
+echo "===================================="
+echo "Membuat Konfigurasi Server"
+echo "===================================="
+cat > /etc/ppp/pptpd-options <<END
+name pptpd
+refuse-pap
+refuse-chap
+refuse-mschap
+require-mschap-v2
+require-mppe-128
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4
+proxyarp
+nodefaultroute
+lock
+nobsdcomp
+END
 
-VPN_LOCAL="192.168.0.150"
-VPN_REMOTE="192.168.0.151-200"
+# setting up pptpd.conf
+echo "option /etc/ppp/pptpd-options" > /etc/pptpd.conf
+echo "logwtmp" >> /etc/pptpd.conf
+echo "localip $ip" >> /etc/pptpd.conf
+echo "remoteip 10.1.0.1-100" >> /etc/pptpd.conf
 
-apt-get -y groupinstall "Development Tools"
-rpm -Uvh http://poptop.sourceforge.net/yum/stable/rhel6/pptp-release-current.noarch.rpm
-apt-get -y install policycoreutils policycoreutils
-apt-get -y install ppp pptpd
-apt-get -y update
+# adding new user
+echo "$u	*	$p	*" >> /etc/ppp/chap-secrets
 
-echo "1" > /proc/sys/net/ipv4/ip_forward
-sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+echo
+echo echo "===================================="
+echo "Mengalihkan IPv4 dan menerapkan saat boot"
+echo echo "===================================="
+cat >> /etc/sysctl.conf <<END
+net.ipv4.ip_forward=1
+END
+sysctl -p
 
-sysctl -p /etc/sysctl.conf
+echo
+echo "===================================="
+echo "Update iptables routing dan menerapkan saat boot"
+echo "===================================="
+iptables -t nat -A POSTROUTING -j SNAT --to $ip
+# saves iptables routing rules and enables them on-boot
+iptables-save > /etc/iptables.conf
 
-echo "localip $VPN_LOCAL" >> /etc/pptpd.conf # Local IP address of your VPN server
-echo "remoteip $VPN_REMOTE" >> /etc/pptpd.conf # Scope for your home network
+cat > /etc/network/if-pre-up.d/iptables <<END
+#!/bin/sh
+iptables-restore < /etc/iptables.conf
+END
 
-echo "ms-dns 8.8.8.8" >> /etc/ppp/options.pptpd # Google DNS Primary
-echo "ms-dns 209.244.0.3" >> /etc/ppp/options.pptpd # Level3 Primary
-echo "ms-dns 208.67.222.222" >> /etc/ppp/options.pptpd # OpenDNS Primary
+chmod +x /etc/network/if-pre-up.d/iptables
+cat >> /etc/ppp/ip-up <<END
+ifconfig ppp0 mtu 1400
+END
 
-echo "$VPN_USER pptpd $VPN_PASS *" >> /etc/ppp/chap-secrets
+echo
+echo "===================================="
+echo "Restart PoPToP"
+echo "===================================="
+sleep 5
+/etc/init.d/pptpd restart
 
-service iptables start
-echo "iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> /etc/rc.local
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-service iptables save
-service iptables restart
+echo
+echo "===================================="
+echo "Sudah Selesai"
+echo "Silahkan konek VPN PPTP $ip dengan user & pass berikut:"
+echo "Username:$u ##### Password: $p"
+echo "===================================="
 
-service pptpd restart
-chkconfig pptpd on
+echo "Script Edited By Dany Meinaki"
 
-echo -e '\E[37;44m'"\033[1m Installation Log: /var/log/vpn-installer.log \033[0m"
-echo -e '\E[37;44m'"\033[1m You can now connect to your VPN via your external IP ($VPN_IP)\033[0m"
+# runs this if option 2 is selected
+elif test $x -eq 2; then
+	echo "Masukan username yang akan di buat (Contoh : Dany, Meinaki):"
+	read u
+	echo "Masukan Password nya:"
+	read p
 
-echo -e '\E[37;44m'"\033[1m Username: $VPN_USER\033[0m"
-echo -e '\E[37;44m'"\033[1m Password: $VPN_PASS\033[0m"
+# get the VPS IP
+ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
 
-) 2>&1 | tee /var/log/vpn-installer.log
+# adding new user
+echo "$u	*	$p	*" >> /etc/ppp/chap-secrets
+
+echo
+echo "===================================="
+echo "User PPTP Sukses di buat!"
+echo "Silahkan konek VPN PPTP $ip dengan user & pass berikut:"
+echo "Username:$u ##### Password: $p"
+echo "===================================="
+
+echo "Script Edited By Dany Meinaki"
+
+else
+echo "Invalid selection, quitting."
+exit
+fi
