6af7bf8ad1143c5ac4839439568b17bd3e6ba333
NetzoneSSH <meinaki@hackermail.com>
2014-06-25 23:26:40 +0700

    Update ovz-deb.sh

--- ovz-deb.sh
+++ ovz-deb.sh
@@ -13,10 +13,6 @@ cd
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
-# install PPTP
-wget https://raw.githubusercontent.com/Naskleng/Bumbu_pepes/master/deb-pptp.sh
-chmod +x deb-pptp.sh && deb-pptp.sh
-
 # install wget and curl
 apt-get update;apt-get -y install wget curl;
 
@@ -161,6 +157,10 @@ apt-get -y -f install;
 rm /root/webmin_1.690_all.deb
 service webmin restart
 service vnstat restart
+
+# install PPTP
+wget https://raw.githubusercontent.com/Naskleng/Bumbu_pepes/master/deb-pptp.sh
+chmod +x deb-pptp.sh && deb-pptp.sh
 
 # download script
 cd
